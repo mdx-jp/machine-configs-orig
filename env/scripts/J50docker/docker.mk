88662034652ed4295989bfe41cb8d0e85deb4991
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2021-07-01 08:06:32 +0900

    docker

--- env/scripts/J50docker/docker.mk
+++ env/scripts/J50docker/docker.mk
@@ -11,7 +11,7 @@ OK : /usr/bin/docker
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 /etc/apt/sources.list.d/docker.list :
-	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(shell lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 /usr/bin/docker : /usr/share/keyrings/docker-archive-keyring.gpg /etc/apt/sources.list.d/docker.list
 	$(apt) update
