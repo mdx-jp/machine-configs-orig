7eb840d9e35025be0ebb773b79d6355e96e2bb53
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2021-07-01 08:04:57 +0900

    docker

--- env/scripts/J50docker/docker.mk
+++ env/scripts/J50docker/docker.mk
@@ -6,13 +6,13 @@ include ../common.mk
 OK : /usr/bin/docker
 
 /usr/share/keyrings/docker-archive-keyring.gpg :
-	apt-get update
-	apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+	$(apt) update
+	$(aptinst) apt-transport-https ca-certificates curl gnupg lsb-release
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 /etc/apt/sources.list.d/docker.list :
 	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 /usr/bin/docker : /usr/share/keyrings/docker-archive-keyring.gpg /etc/apt/sources.list.d/docker.list
-	sudo apt-get update
-	sudo apt-get install docker-ce docker-ce-cli containerd.io
+	$(apt) update
+	$(apt) install docker-ce docker-ce-cli containerd.io
